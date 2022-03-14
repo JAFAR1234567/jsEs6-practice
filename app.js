88eb1dc19366e0498103ef51ca83fc8f067b6295
abu jafar <you@example.com>
2022-03-14 10:12:36 +0600

    es6 practioce

---
+++ app.js
@@ -0,0 +1,16 @@
+
+// const button = document.getElementById("button");
+// button.addEventListener('click', () => {
+//     const input = document.getElementById('input-item');
+//     const inputValue = input.value;
+//     if(isNaN(inputValue)){
+//         console.log(typeof inputValue,'not a number',inputValue);
+//     }
+//     else{
+//         console.log(typeof inputValue,inputValue);
+//     }
+    
+//     input.value = '';
+// })
+
+
